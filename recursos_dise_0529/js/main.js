7ab68ffb8b033ccf17c835b7b469a784ac9c8318
tjc-jireh <50995947+tjc-jireh@users.noreply.github.com>
2021-11-25 11:46:12 -0500

    Delete recursos_dise_0529 directory

--- recursos_dise_0529/js/main.js
+++
@@ -1,8 +0,0 @@
-function navigation(capa,enlace)  
-{  
-    if (document.getElementById)  
-    {  
-        var aux = document.getElementById(capa).style;  
-        aux.display = aux.display? "":"block";  
-    }  
-} 
